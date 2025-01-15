1d85f5deaa5229fc351141b5cd5ff02369224f73
Rotem Heinig <rotemheinig@icloud.com>
2025-01-15 21:13:50 +0200

    Add project start

---
+++ app/src/main/java/com/colman/colman_kotlin_tictactoe/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.colman.colman_kotlin_tictactoe
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.colman.colman_kotlin_tictactoe.ui.theme.ColmankotlintictactoeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ColmankotlintictactoeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ColmankotlintictactoeTheme {
+        Greeting("Android")
+    }
+}
